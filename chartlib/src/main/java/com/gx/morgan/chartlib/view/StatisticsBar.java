67d8038c2cee7fcedc419092fff88ab8c2b7911b
caowugao <caowugao@gmail.com>
2018-04-07 16:59:44 +0800

    为柱状图添加动画

--- chartlib/src/main/java/com/gx/morgan/chartlib/view/StatisticsBar.java
+++ chartlib/src/main/java/com/gx/morgan/chartlib/view/StatisticsBar.java
@@ -1,6 +1,8 @@
 package com.gx.morgan.chartlib.view;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,6 +11,9 @@ import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.util.SparseIntArray;
+import android.view.animation.DecelerateInterpolator;
 
 import com.gx.morgan.chartlib.utils.MathUtil;
 import com.gx.morgan.chartlib.utils.ViewUtil;
@@ -50,8 +55,10 @@ public class StatisticsBar extends CustomView {
     private int unitDescTextSize;
     private int unitDescTextColor;
 
-    private boolean animate=true;
-    private List<Animator> animators;
+    private boolean animate = true;//是否需要动画
+    private SparseIntArray heightValueMap;
+    private SparseArray<ValueAnimator> animatorMap;
+    private boolean animateStarted=false;//动画是否已经开始
 
     public static class XCoordinateBulgeDirection {
         private XCoordinateBulgeDirection() {
@@ -62,10 +69,11 @@ public class StatisticsBar extends CustomView {
     }
 
     public static class ContentData {
-        public ContentData(int x, int y){
-            this.x=x;
-            this.y=y;
+        public ContentData(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
+
         public int x;
         public int y;
     }
@@ -99,7 +107,7 @@ public class StatisticsBar extends CustomView {
         yTextSize = xTextSize;
         yTextColor = xTextColor;
         statisticBarColor = Color.BLUE;
-        statisticBarWidth= (int) ViewUtil.dp2px(context,10);
+        statisticBarWidth = (int) ViewUtil.dp2px(context, 10);
         textCoordinatePadding = (int) ViewUtil.dp2px(context, 5);
         textBound = new Rect();
         contentPadding = (int) ViewUtil.dp2px(context, 5);
@@ -111,13 +119,12 @@ public class StatisticsBar extends CustomView {
         arrowWidth = arrowHeight;
 
         dp2 = ViewUtil.dp2px(context, 2);
-        unitDescTextSize= (int) ViewUtil.sp(context,14);
-        unitDescTextColor=yTextColor;
+        unitDescTextSize = (int) ViewUtil.sp(context, 14);
+        unitDescTextColor = yTextColor;
 
 
-        xCoodinateUnitDesc="月份";
-        yCoodinateUnitDesc="台";
-
+        xCoodinateUnitDesc = "月份";
+        yCoodinateUnitDesc = "台";
 
 
         int xDataSize = 8;
@@ -133,13 +140,48 @@ public class StatisticsBar extends CustomView {
             yCoodinateDatas.add(8 * i);
         }
 
-        int contentDataSize=8;
-        contentDatas=new ArrayList<>(contentDataSize);
-        int[] datas=new int[]{24,48,40,8,0,16,32,48};
-        for (int i = 0; i <contentDataSize ; i++) {
-            contentDatas.add(new ContentData(i+1,datas[i]));
+
+        final int contentDataSize = 8;
+        contentDatas = new ArrayList<>(contentDataSize);
+        int[] datas = new int[]{24, 48, 40, 8, 0, 16, 32, 48};
+        for (int i = 0; i < contentDataSize; i++) {
+            ContentData contentData = new ContentData(i + 1, datas[i]);
+            contentDatas.add(contentData);
+        }
+
+
+        if(animate){
+
+            animatorMap = new SparseArray<>();
+            heightValueMap = new SparseIntArray();
+
+            for (int i = 0; i <contentDataSize ; i++) {
+                ValueAnimator animator = ValueAnimator.ofInt();
+                animator.setDuration(500);
+                animator.setInterpolator(new DecelerateInterpolator());
+                final int finalI = i;
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        int baeHeightValue = (int) valueAnimator.getAnimatedValue();
+                        heightValueMap.put(finalI,baeHeightValue);
+                        invalidate();
+                    }
+                });
+                final int finalI1 = i;
+                animator.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation, boolean isReverse) {
+                        if(finalI1 ==contentDataSize-1){
+                            animateStarted=false;
+                        }
+                    }
+                });
+                animatorMap.put(i,animator);
+            }
         }
     }
+
 
     @Override
     protected int contentWidth(int widthMeasureSpec) {
@@ -158,6 +200,15 @@ public class StatisticsBar extends CustomView {
         height = h;
     }
 
+    @Override
+    protected void onDetachedFromWindow() {
+        contentDatas.clear();
+        heightValueMap.clear();
+        animatorMap.clear();
+        super.onDetachedFromWindow();
+
+    }
+
     public void setBarWidth(int barWidth) {
 //        param.setBarWidth(barWidth);
         invalidate();
@@ -171,7 +222,7 @@ public class StatisticsBar extends CustomView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(Color.GRAY);
+//        canvas.drawColor(Color.GRAY);
         drawStatisticBar(canvas);
     }
 
@@ -211,8 +262,8 @@ public class StatisticsBar extends CustomView {
         int xCoodinateBuglyStopY = 0;
 
 
-        int arrowUpX=0;//x轴三角形x坐标
-        int arrowUpY=0;//x轴三角形y坐标
+        int arrowUpX = 0;//x轴三角形x坐标
+        int arrowUpY = 0;//x轴三角形y坐标
         for (int i = 0, size = xDataSize; i < size; i++) {
             //画x轴数字
             paint.setColor(xTextColor);
@@ -236,8 +287,8 @@ public class StatisticsBar extends CustomView {
             }
 
             if (i == size - 1) {//最后一个就画箭头
-                 arrowUpX = coodinateXStopX - arrowWidth;
-                 arrowUpY = coodinateXStartY - arrowHeight;
+                arrowUpX = coodinateXStopX - arrowWidth;
+                arrowUpY = coodinateXStartY - arrowHeight;
                 paint.setStrokeJoin(Paint.Join.MITER);//设置拐角为尖的
                 canvas.drawLine(coodinateXStopX, coodinateXStopY, arrowUpX, arrowUpY, paint);
 
@@ -263,27 +314,27 @@ public class StatisticsBar extends CustomView {
         canvas.drawLine(coodinateYStartX, coodinateYStartY, coodinateYStopX, coodinateYStopY, paint);
 
         //画y轴箭头
-        int arrowLeftX=coodinateYStopX-arrowHeight;
-        int arrowLeftY=coodinateYStopY+arrowWidth;
-        int arrowRightX=coodinateYStopX+arrowHeight;
-        int arrowRightY=arrowLeftY;
-        canvas.drawLine(coodinateYStopX,coodinateYStopY,arrowLeftX,arrowLeftY,paint);
-        canvas.drawLine(coodinateYStopX,coodinateYStopY,arrowRightX,arrowRightY,paint);
+        int arrowLeftX = coodinateYStopX - arrowHeight;
+        int arrowLeftY = coodinateYStopY + arrowWidth;
+        int arrowRightX = coodinateYStopX + arrowHeight;
+        int arrowRightY = arrowLeftY;
+        canvas.drawLine(coodinateYStopX, coodinateYStopY, arrowLeftX, arrowLeftY, paint);
+        canvas.drawLine(coodinateYStopX, coodinateYStopY, arrowRightX, arrowRightY, paint);
 
         //画y轴单位说明
-        if(!TextUtils.isEmpty(yCoodinateUnitDesc)){
+        if (!TextUtils.isEmpty(yCoodinateUnitDesc)) {
             paint.setColor(unitDescTextColor);
             paint.setTextSize(unitDescTextSize);
-            paint.getTextBounds(yCoodinateUnitDesc,0,yCoodinateUnitDesc.length(),textBound);
-            textStartX =  coodinateXStartX+textCoordinatePadding;
-            textStartY=arrowLeftY+textBound.height();
-            canvas.drawText(yCoodinateUnitDesc,textStartX,textStartY,paint);
+            paint.getTextBounds(yCoodinateUnitDesc, 0, yCoodinateUnitDesc.length(), textBound);
+            textStartX = coodinateXStartX + textCoordinatePadding;
+            textStartY = arrowLeftY + textBound.height();
+            canvas.drawText(yCoodinateUnitDesc, textStartX, textStartY, paint);
         }
 
 
         int paddingLeft = getPaddingLeft();
         int yDataSize = yCoodinateDatas.size();
-        int yCoodinateDistance =coodinateYStartY - coodinateYStopY;
+        int yCoodinateDistance = coodinateYStartY - coodinateYStopY;
         int ySpacing = yCoodinateDistance / yDataSize;
 
         int contentLineStartX = 0;
@@ -311,56 +362,168 @@ public class StatisticsBar extends CustomView {
             contentLineStopX = coodinateXStopX;
             contentLineStopY = contentLineStartY;
 
-            canvas.drawLine(contentLineStartX,contentLineStartY,contentLineStopX,contentLineStopY,paint);
+            canvas.drawLine(contentLineStartX, contentLineStartY, contentLineStopX, contentLineStopY, paint);
         }
-
 
 
         //画柱状图
-        int xCoodinateMaxData=xCoodinateDatas.get(xDataSize-1);//x轴最大数字
-        int yCoodinateMaxData= yCoodinateDatas.get(yDataSize-1)+yCoodinateDatas.get(yDataSize-1)-yCoodinateDatas.get(yDataSize-2);//y轴最大数字
-        int statisicBarHeight=0;
-
+        int xCoodinateMaxData = xCoodinateDatas.get(xDataSize - 1);//x轴最大数字
+        int yCoodinateMaxData = yCoodinateDatas.get(yDataSize - 1) + yCoodinateDatas.get(yDataSize - 1) -
+                yCoodinateDatas.get(yDataSize - 2);//y轴最大数字
+        int statisicBarHeight = 0;
 
         paint.setTextAlign(Paint.Align.LEFT);
 
-        for (int i = 0,size=contentDatas.size(); i <size ; i++) {
+        if(animateStarted){//动画是否已经开始
 
-            //画柱状
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(statisticBarColor);
-            ContentData contentData = contentDatas.get(i);
-            int left= (int) (coodinateXStartX+(contentData.x*1.0/xCoodinateMaxData)*xCoordinateDistance-xSpacing/2-statisticBarWidth/2);
-            statisicBarHeight= (int) ((contentData.y*1.0/yCoodinateMaxData)*yCoodinateDistance);
-            statisicBarHeight= 0==statisicBarHeight? (int) dp2 :statisicBarHeight;//柱状高度为0就附上2dp
-            int top=coodinateXStartY-statisicBarHeight;
-            int right=left+statisticBarWidth;
-            int bottom=coodinateXStartY;
-            canvas.drawRect( left,  top,  right,  bottom,paint);
+            for (int i = 0, size = contentDatas.size(); i < size; i++) {
+
+                //画柱状
+                ContentData contentData = contentDatas.get(i);
+//                statisicBarHeight = (int) ((contentData.y * 1.0 / yCoodinateMaxData) * yCoodinateDistance);
+//                statisicBarHeight = 0 == statisicBarHeight ? (int) dp2 : statisicBarHeight;//柱状高度为0就附上2dp
+
+                statisicBarHeight = heightValueMap.get(i);
+                drawSingleBar(canvas, coodinateXStartX, coodinateXStartY, xCoordinateDistance, xSpacing,
+                        xCoodinateMaxData, statisicBarHeight, contentData);
+
+            }
+        }
+        else {//动画还没开始
+            animateStarted=true;
+            for (int i = 0, size = contentDatas.size(); i < size; i++) {
+
+                //画柱状
+                ContentData contentData = contentDatas.get(i);
+                statisicBarHeight = (int) ((contentData.y * 1.0 / yCoodinateMaxData) * yCoodinateDistance);
+                statisicBarHeight = 0 == statisicBarHeight ? (int) dp2 : statisicBarHeight;//柱状高度为0就附上2dp
+
+                ValueAnimator animator = animatorMap.get(i);
+                if(null!=animator){
+                    if(animator.isRunning()){
+                        animator.cancel();
+                    }
+                    animator.setIntValues(0,statisicBarHeight);
+                }
+            }
 
 
-            //画柱状图顶部的数字
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setTextSize(yTextSize);
-            paint.setColor(yTextColor);
-            String text=String.valueOf(contentData.y);
-            paint.getTextBounds(text,0,text.length(),textBound);
-            int textWidth=textBound.width();
-            textStartX=left+statisticBarWidth/2-textWidth/2;
-            textStartY=top-textCoordinatePadding;
-            canvas.drawText(text,textStartX,textStartY,paint);
+            for (int i = 0, size = contentDatas.size(); i < size; i++) {
+                ValueAnimator animator = animatorMap.get(i);
+                if(null!=animator){
+                    animator.start();
+                }
+            }
         }
 
         //画x轴单位说明
-        if(!TextUtils.isEmpty(xCoodinateUnitDesc)){
+        if (!TextUtils.isEmpty(xCoodinateUnitDesc)) {
             paint.setTextSize(unitDescTextSize);
             paint.setColor(unitDescTextColor);
-            paint.getTextBounds(xCoodinateUnitDesc,0,xCoodinateUnitDesc.length(),textBound);
-            textStartX=arrowUpX-textBound.width();
-            textStartY=arrowUpY;
-            canvas.drawText(xCoodinateUnitDesc,textStartX,textStartY,paint);
+            paint.getTextBounds(xCoodinateUnitDesc, 0, xCoodinateUnitDesc.length(), textBound);
+            textStartX = arrowUpX - textBound.width();
+            textStartY = arrowUpY;
+            canvas.drawText(xCoodinateUnitDesc, textStartX, textStartY, paint);
         }
     }
+
+    private void drawSingleBar(Canvas canvas, int coodinateXStartX, int coodinateXStartY, int xCoordinateDistance,
+                               int xSpacing, int xCoodinateMaxData, int barHeight, ContentData contentData) {
+        int textStartX;
+        int textStartY;
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(statisticBarColor);
+        int left = (int) (coodinateXStartX + (contentData.x * 1.0 / xCoodinateMaxData) * xCoordinateDistance -
+                xSpacing / 2 - statisticBarWidth / 2);
+        int top = coodinateXStartY - barHeight;
+        int right = left + statisticBarWidth;
+        int bottom = coodinateXStartY;
+        canvas.drawRect(left, top, right, bottom, paint);
+
+
+        //画柱状图顶部的数字
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setTextSize(yTextSize);
+        paint.setColor(yTextColor);
+        String text = String.valueOf(contentData.y);
+        paint.getTextBounds(text, 0, text.length(), textBound);
+        int textWidth = textBound.width();
+        textStartX = left + statisticBarWidth / 2 - textWidth / 2;
+        textStartY = top - textCoordinatePadding;
+        canvas.drawText(text, textStartX, textStartY, paint);
+    }
+
+//    private void drawSingleBar(final Canvas canvas, final int coodinateXStartX, final int coodinateXStartY, final
+// int xCoordinateDistance,
+//                               final int xSpacing, final int xCoodinateMaxData, final int statisicBarHeight, final
+// ContentData contentData) {
+//
+//        if(!animate){//没有动画
+//            paint.setStyle(Paint.Style.FILL);
+//            paint.setColor(statisticBarColor);
+//            int textStartX;
+//            int textStartY;
+//            int left= (int) (coodinateXStartX+(contentData.x*1.0/xCoodinateMaxData)
+// *xCoordinateDistance-xSpacing/2-statisticBarWidth/2);
+//            int top=coodinateXStartY-statisicBarHeight;
+//            int right=left+statisticBarWidth;
+//            int bottom=coodinateXStartY;
+//            canvas.drawRect( left,  top,  right,  bottom,paint);
+//
+//
+//            //画柱状图顶部的数字
+//            paint.setStyle(Paint.Style.STROKE);
+//            paint.setTextSize(yTextSize);
+//            paint.setColor(yTextColor);
+//            String text=String.valueOf(contentData.y);
+//            paint.getTextBounds(text,0,text.length(),textBound);
+//            int textWidth=textBound.width();
+//            textStartX=left+statisticBarWidth/2-textWidth/2;
+//            textStartY=top-textCoordinatePadding;
+//            canvas.drawText(text,textStartX,textStartY,paint);
+//        }
+//        else {//动画
+//            ValueAnimator animator=ValueAnimator.ofInt(0,statisicBarHeight/6,statisicBarHeight/3,statisicBarHeight);
+//            animator.setDuration(500);
+//            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//                @Override
+//                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+//                    int value = (int) valueAnimator.getAnimatedValue();
+//                    drawSingleBarWithAnimate( canvas,  coodinateXStartX,  coodinateXStartY,  xCoordinateDistance,
+//                     xSpacing,  xCoodinateMaxData,  value,  contentData);
+//                }
+//            });
+//            animator.start();
+//        }
+//    }
+
+//    private void drawSingleBarWithAnimate(Canvas canvas, int coodinateXStartX, int coodinateXStartY, int
+// xCoordinateDistance,
+//                                          int xSpacing, int xCoodinateMaxData, int barHeightAnimateValue,
+// ContentData contentData) {
+//        paint.setStyle(Paint.Style.FILL);
+//        paint.setColor(statisticBarColor);
+//        int textStartX;
+//        int textStartY;
+//        int left= (int) (coodinateXStartX+(contentData.x*1.0/xCoodinateMaxData)
+// *xCoordinateDistance-xSpacing/2-statisticBarWidth/2);
+//        int top=coodinateXStartY-barHeightAnimateValue;
+//        int right=left+statisticBarWidth;
+//        int bottom=coodinateXStartY;
+//        canvas.drawRect( left,  top,  right,  bottom,paint);
+//
+//
+//        //画柱状图顶部的数字
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setTextSize(yTextSize);
+//        paint.setColor(yTextColor);
+//        String text=String.valueOf(contentData.y);
+//        paint.getTextBounds(text,0,text.length(),textBound);
+//        int textWidth=textBound.width();
+//        textStartX=left+statisticBarWidth/2-textWidth/2;
+//        textStartY=top-textCoordinatePadding;
+//        canvas.drawText(text,textStartX,textStartY,paint);
+//    }
 
     private float getMaxTextWidth(List coodinateDatas, Paint paint) {
         float[] widths = new float[coodinateDatas.size()];
