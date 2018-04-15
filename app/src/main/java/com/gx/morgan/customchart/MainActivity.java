1f8ca1e3812f64844f15610219c1607bb7eaf52e
caowugao <caowugao@gmail.com>
2018-04-15 19:07:23 +0800

    添加折线图

--- app/src/main/java/com/gx/morgan/customchart/MainActivity.java
+++ app/src/main/java/com/gx/morgan/customchart/MainActivity.java
@@ -3,6 +3,8 @@ package com.gx.morgan.customchart;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.gx.morgan.chartlib.view.BaseCoordinateView;
+import com.gx.morgan.chartlib.view.LineView;
 import com.gx.morgan.chartlib.view.StatisticsBar;
 
 import java.util.ArrayList;
@@ -16,11 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        StatisticsBar statisticsBar= findViewById(R.id.statisticsBar);
+        initStatisticBar();
+        initLineView();
+    }
 
+    private void initLineView() {
+        LineView lineView = findViewById(R.id.lineView);
         //x轴数据
         int xDataSize = 8;
-       List<Integer> xCoodinateDatas = new ArrayList<>(xDataSize);
+        List<Integer> xCoodinateDatas = new ArrayList<>(xDataSize);
         for (int i = 0; i < xDataSize; i++) {
             xCoodinateDatas.add(i + 1);
         }
@@ -36,13 +42,44 @@ public class MainActivity extends AppCompatActivity {
 
         //柱状图数据
         int contentDataSize = 8;
-        List<StatisticsBar.ContentData>contentDatas = new ArrayList<>(contentDataSize);
+        List<StatisticsBar.ContentData> contentDatas = new ArrayList<>(contentDataSize);
         int[] datas = new int[]{24, 48, 40, 8, 0, 16, 32, 48};
         for (int i = 0; i < contentDataSize; i++) {
-            StatisticsBar.ContentData contentData = new StatisticsBar.ContentData(i + 1, datas[i]);
+            BaseCoordinateView.ContentData contentData = new BaseCoordinateView.ContentData(i + 1, datas[i]);
             contentDatas.add(contentData);
         }
 
-        statisticsBar.setFullData(xCoodinateDatas,yCoodinateDatas,contentDatas);
+
+        lineView.setFullData(xCoodinateDatas, yCoodinateDatas, contentDatas);
+    }
+
+    private void initStatisticBar() {
+        StatisticsBar statisticsBar = findViewById(R.id.statisticsBar);
+        //x轴数据
+        int xDataSize = 8;
+        List<Integer> xCoodinateDatas = new ArrayList<>(xDataSize);
+        for (int i = 0; i < xDataSize; i++) {
+            xCoodinateDatas.add(i + 1);
+        }
+
+
+        //y轴数据
+        int yDataSize = 7;
+        List<Integer> yCoodinateDatas = new ArrayList<>(yDataSize);
+        for (int i = 0; i < yDataSize; i++) {
+            yCoodinateDatas.add(8 * i);
+        }
+
+
+        //柱状图数据
+        int contentDataSize = 8;
+        List<StatisticsBar.ContentData> contentDatas = new ArrayList<>(contentDataSize);
+        int[] datas = new int[]{24, 48, 40, 8, 0, 16, 32, 48};
+        for (int i = 0; i < contentDataSize; i++) {
+            BaseCoordinateView.ContentData contentData = new BaseCoordinateView.ContentData(i + 1, datas[i]);
+            contentDatas.add(contentData);
+        }
+
+        statisticsBar.setFullData(xCoodinateDatas, yCoodinateDatas, contentDatas);
     }
 }
--- chartlib/src/main/java/com/gx/morgan/chartlib/view/LineView.java
+++ chartlib/src/main/java/com/gx/morgan/chartlib/view/LineView.java
@@ -1,78 +1,117 @@
 package com.gx.morgan.chartlib.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
 import com.gx.morgan.chartlib.utils.ViewUtil;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.util.List;
 
 /**
  * description：折线图
  * <br>author：caowugao
  * <br>time：2018/4/15 14:12
  */
-public class LineView extends CustomView {
+public class LineView extends BaseCoordinateView {
 
 
-    public static class XCoordinateBulgeDirection {
-        private XCoordinateBulgeDirection() {
-        }
+    private int pointRadius;
+    private int lineColor;
+    private boolean isPointSolid = false;
 
-        public static final int UP = 1;
-        public static final int DOWN = 2;
-    }
-
-    @Retention(RetentionPolicy.CLASS)
-    @IntDef({XCoordinateBulgeDirection.UP, XCoordinateBulgeDirection.DOWN})
-    public @interface XCoordinateBulgeDirectionType {
-    }
-
-    public static class ContentData {
-        public ContentData(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int x;
-        public int y;
-    }
     public LineView(Context context) {
         super(context);
+        init(context, null);
     }
 
     public LineView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
+
 
     public LineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs);
     }
 
     public LineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        pointRadius = (int) ViewUtil.dp2px(context, 5);
+        lineColor = Color.BLUE;
+        isPointSolid = false;
     }
 
 
     @Override
-    protected int contentWidth(int widthMeasureSpec) {
-        return (int) ViewUtil.dp2px(getContext(), 300);
+    public void initAnimator(boolean animate, List<ContentData> contentDatas) {
+
+
     }
 
     @Override
-    protected int contentHeight(int heightMeasureSpec) {
-        return (int) ViewUtil.dp2px(getContext(), 350);
+    protected boolean isXCoordinateDataInBulge() {
+        return true;
     }
 
-    private static float getTextWidth(String text, Paint paint) {
-        return paint.measureText(text);
-    }
+    @Override
+    protected void onDrawSelfContent(Canvas canvas, int coordinateXStartX, int coordinateXStartY, int coordinateXStopX,
+                                     int coordinateXStopY, int coordinateYStartX, int coordinateYStartY, int
+                                             coordinateYStopX, int coordinateYStopY) {
 
-    private static float getTextHeight(Paint paint) {
-        return (-paint.ascent() + paint.descent());
+
+        int xCoordinateDistance = Math.abs(coordinateXStartX - coordinateXStopX);
+        int yCoordinateDistance = Math.abs(coordinateYStartY - coordinateYStopY);
+
+        int xDataSize = xCoordinateDatas.size();
+        int xCoordinateDataRangeLength = xCoordinateDatas.get(xDataSize - 1) - xCoordinateDatas.get(0);//x轴数据区间长度
+
+        int yDataSize = yCoordinateDatas.size();
+        int yCoodinateDataRangLength = yCoordinateDatas.get(yDataSize - 1) + yCoordinateDatas.get(yDataSize - 1) -
+                yCoordinateDatas.get(yDataSize - 2) - yCoordinateDatas.get(0);//y轴数据区间长度
+
+        paint.setColor(lineColor);
+        paint.setStyle(Paint.Style.STROKE);
+
+
+        float startX = 0;
+        float startY = 0;
+        float stopX = 0;
+        float stopY = 0;
+        float pointX = 0;
+        float pointY = 0;
+        float prePointX = 0;
+        float prePointY = 0;
+        float oldStrokeWidth = paint.getStrokeWidth();
+        for (int i = 0, size = contentDatas.size(); i < size; i++) {
+            ContentData contentData = contentDatas.get(i);
+            pointX = (float) (coordinateXStartX + xCoordinateDistance * contentData.x * 1.0 /
+                    xCoordinateDataRangeLength);
+            pointY = (float) (coordinateYStopY + yCoordinateDistance * contentData.y * 1.0 / yCoodinateDataRangLength);
+            paint.setStrokeWidth(2 * pointRadius);
+            canvas.drawPoint(pointX, pointY, paint);
+            if (0 == i) {
+                prePointX = pointX;
+                prePointY = pointY;
+            } else {
+                paint.setStrokeWidth(oldStrokeWidth);
+                startX = prePointX;
+                startY = prePointY;
+                stopX = pointX;
+                stopY = pointY;
+                canvas.drawLine(startX, startY, stopX, stopY, paint);
+
+                prePointX = pointX;
+                prePointY = pointY;
+            }
+        }
     }
 }
