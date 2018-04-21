c6903788f77a0e27b5ac189f91fcd17de00a952f
caowugao <caowugao@gmail.com>
2018-04-21 18:12:25 +0800

    将dimension的变量改为float

--- chartlib/src/main/java/com/gx/morgan/chartlib/view/LineView.java
+++ chartlib/src/main/java/com/gx/morgan/chartlib/view/LineView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
@@ -21,14 +23,19 @@ public class LineView extends BaseCoordinateView {
 
     private int pointRadius;
     private int lineColor;
-    private boolean isPointSolid = false;
-
     private int pointType;
+
+    private int contentTextSize;
+    private int contextTextColor;
+
+    private int roundDegree;
+    private RectF pointRectF;
+    private int lineWidth;
 
     public static class PointType {
         public static final int CIRCLE = 1;
         public static final int RECT = 2;
-        public static final int SOLIDROUND = 3;
+        public static final int ROUNDRECT = 3;
     }
 
     public LineView(Context context) {
@@ -54,9 +61,13 @@ public class LineView extends BaseCoordinateView {
 
     private void init(Context context, AttributeSet attrs) {
         pointRadius = (int) ViewUtil.dp2px(context, 5);
+        contentTextSize= (int) ViewUtil.sp(context,12);
+        contextTextColor=Color.RED;
         lineColor = Color.BLUE;
-        isPointSolid = false;
-        pointType = PointType.CIRCLE;
+        pointType = PointType.ROUNDRECT;
+        roundDegree = (int) ViewUtil.dp2px(context, 2);
+        lineWidth = (int) ViewUtil.dp2px(context, 1);
+        pointRectF = new RectF();
     }
 
 
@@ -72,31 +83,27 @@ public class LineView extends BaseCoordinateView {
     }
 
     @Override
-    protected void onDrawSelfContent(Canvas canvas, int coordinateXStartX, int coordinateXStartY, int coordinateXStopX,
-                                     int coordinateXStopY, int coordinateYStartX, int coordinateYStartY, int
-                                             coordinateYStopX, int coordinateYStopY) {
+    protected void onDrawSelfContent(Canvas canvas, float coordinateXStartX, float coordinateXStartY, float coordinateXStopX,
+                                     float coordinateXStopY, float coordinateYStartX, float coordinateYStartY, float
+                                             coordinateYStopX, float coordinateYStopY) {
 
 
-        int xCoordinateDistance = Math.abs(coordinateXStartX - coordinateXStopX);
-        int yCoordinateDistance = Math.abs(coordinateYStartY - coordinateYStopY);
+        float xCoordinateDistance = Math.abs(coordinateXStartX - coordinateXStopX);//x轴长度
+        float yCoordinateDistance = Math.abs(coordinateYStartY - coordinateYStopY);//y轴长度
 
 
         int firstXCoordinateData = xCoordinateDatas.get(0);
         int xDataSize = xCoordinateDatas.size();
         int xCoordinateDataRangeLength = xCoordinateDatas.get(xDataSize - 1) + xCoordinateDatas.get(xDataSize - 1) -
                 xCoordinateDatas.get(xDataSize - 2) - firstXCoordinateData;//x轴数据区间长度
-//        int xCoordinateDataRangeLength = xCoordinateDatas.get(xDataSize - 1);//x轴数据区间长度
 
         int yDataSize = yCoordinateDatas.size();
         int firstYCoordinateData = yCoordinateDatas.get(0);
         int yCoodinateDataRangLength = yCoordinateDatas.get(yDataSize - 1) + yCoordinateDatas.get(yDataSize - 1) -
                 yCoordinateDatas.get(yDataSize - 2) - firstYCoordinateData;//y轴数据区间长度
-//        int yCoodinateDataRangLength = yCoordinateDatas.get(yDataSize - 1) + yCoordinateDatas.get(yDataSize - 1) -
-//                yCoordinateDatas.get(yDataSize - 2) ;//y轴数据区间长度
 
-        paint.setColor(lineColor);
         paint.setStyle(Paint.Style.STROKE);
-
+        paint.setStrokeWidth(lineWidth);
 
         float startX = 0;
         float startY = 0;
@@ -113,15 +120,17 @@ public class LineView extends BaseCoordinateView {
             ContentData contentData = contentDatas.get(i);
             pointX = (float) (coordinateXStartX + xCoordinateDistance * (contentData.x - firstXCoordinateData) * 1.0 /
                     xCoordinateDataRangeLength);
-            pointY = yCoordinateDistance-(float) (yCoordinateDistance * (contentData.y - firstYCoordinateData) * 1.0 /
+            pointY = coordinateYStartY - (float) (yCoordinateDistance * (contentData.y - firstYCoordinateData) * 1.0 /
                     yCoodinateDataRangLength);
-            paint.setStrokeWidth(2 * pointRadius);
-            canvas.drawPoint(pointX, pointY, paint);
+
+
+
             if (0 == i) {
                 prePointX = pointX;
                 prePointY = pointY;
             } else {
                 paint.setStrokeWidth(oldStrokeWidth);
+                paint.setColor(lineColor);
                 startX = prePointX;
                 startY = prePointY;
                 stopX = pointX;
@@ -131,6 +140,65 @@ public class LineView extends BaseCoordinateView {
                 prePointX = pointX;
                 prePointY = pointY;
             }
+
+            drawInflectionPoint(canvas, pointX, pointY);//画拐点
+            drawConentText(canvas, pointX, pointY, contentData);
         }
+    }
+
+    private void drawConentText(Canvas canvas, float pointX, float pointY, ContentData contentData) {
+        paint.setTextSize(contentTextSize);
+        paint.setColor(contextTextColor);
+        String text = String.valueOf(contentData.y);
+        paint.getTextBounds(text, 0, text.length(), textBound);
+
+        float x = pointX +textBound.width() / 2;
+        float y = pointY - pointRadius - textCoordinatePadding;
+        canvas.drawText(text, x, y, paint);
+    }
+
+    /**
+     * 画拐点
+     *
+     * @param canvas
+     * @param pointX
+     * @param pointY
+     */
+    private void drawInflectionPoint(Canvas canvas, float pointX, float pointY) {
+        paint.setColor(lineColor);
+        paint.setStyle(Paint.Style.FILL);
+        switch (pointType) {
+            case PointType.CIRCLE:
+                float cx = pointX;
+                float cy = pointY;
+                canvas.drawCircle(cx, cy, pointRadius, paint);
+
+                break;
+            case PointType.RECT:
+                float left = pointX - pointRadius;
+                float top = pointY - pointRadius;
+                float right = pointX + pointRadius;
+                float bottom = pointY + pointRadius;
+                canvas.drawRect(left, top, right, bottom, paint);
+                break;
+            case PointType.ROUNDRECT://圆角方形
+                float rx = roundDegree;
+                float ry = roundDegree;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    left = pointX - pointRadius;
+                    top = pointY - pointRadius;
+                    right = pointX + pointRadius;
+                    bottom = pointY + pointRadius;
+                    canvas.drawRoundRect(left, top, right, bottom, rx, ry, paint);
+                } else {
+                    pointRectF.left = pointX - pointRadius;
+                    pointRectF.top = pointY - pointRadius;
+                    pointRectF.right = pointX + pointRadius;
+                    pointRectF.bottom = pointY + pointRadius;
+                    canvas.drawRoundRect(pointRectF, rx, ry, paint);
+                }
+                break;
+        }
+
     }
 }
